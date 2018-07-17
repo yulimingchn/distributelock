2fbd173dba99c55bf43f837c0d85d9bd00d35777
v_yuliming <yuliming@chinasoftinfo.com>
2018-07-17 22:03:23 +0800

    初始化项目

---
+++ src/main/java/com/dawn/banana/distriutelock/DistriutelockApplication.java
@@ -0,0 +1,12 @@
+package com.dawn.banana.distriutelock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistriutelockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistriutelockApplication.class, args);
+	}
+}
